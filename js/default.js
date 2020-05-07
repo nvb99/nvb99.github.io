751e7848331ed27ece03a645b97ed5a28b903e9d
baynv <bay.nv910@gmail.com>
2020-05-07 22:09:57 +0700

    My web ver 1.0.1

---
+++ js/default.js
@@ -0,0 +1,34 @@
+/* ============= PRE-LOADER ============= */
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-end');
+});
+
+let crDate = new Date();
+document.querySelector('#crTime').innerHTML = crDate.getHours() + ":" + crDate.getMinutes();
+document.querySelector('#crYear').innerHTML = crDate.getFullYear();
+
+/* ============= NAVBAR JS ============= */
+const navSlide = () => {
+	const mobileBar = document.querySelector(".mobileBar");
+	const nav = document.querySelector(".nav-links");
+
+	mobileBar.addEventListener("click", () => {
+		// toggle nav
+		nav.classList.toggle("nav-active");
+
+		// mobileBar animation
+		mobileBar.classList.toggle('toggle');
+	});
+}
+navSlide();
+/* ============= BACKGROUND HOVER EFFECT ============= */
+let $ = jQuery.noConflict();
+jQuery(function($) {
+	"use strict";
+	// STARS
+	$("#star").each(function() {
+		postars($('.cover')[0]).init();
+	});
+});
+/* ============= MAIN JS HERE ============= */

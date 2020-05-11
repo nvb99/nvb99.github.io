8dfcd71ea29d0cdde6ba2e52543c228908240b99
baynv <bay.nv910@gmail.com>
2020-05-12 05:55:36 +0700

    modify some css

--- js/default.js
+++ js/default.js
@@ -22,6 +22,20 @@ const navSlide = () => {
 	});
 }
 navSlide();
+
+//Get the button:
+mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = 1;
+  } else {
+    mybutton.style.opacity = 0;
+  }
+}
 /* ============= BACKGROUND HOVER EFFECT ============= */
 
 let $ = jQuery.noConflict();
